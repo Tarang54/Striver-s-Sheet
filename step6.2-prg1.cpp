3511633eb00a6ac74db486314b8cc1f25f367869
Tarang Sultania <108229445+Tarang54@users.noreply.github.com>
2024-03-24 02:48:06 +0530

    Create step6.2-prg1.cpp

---
+++ step6.2-prg1.cpp
@@ -0,0 +1,25 @@
+/*
+ * Definition for doubly-linked list.
+ * class Node
+ * {
+ * public:
+ *    int data;
+ *    Node *next, *prev;
+ *    Node() : data(0), next(nullptr), prev(nullptr) {}
+ *    Node(int x) : data(x), next(nullptr), prev(nullptr) {}
+ *    Node(int x, Node *next, Node *prev) : data(x), next(next), prev(prev) {}
+ * };
+ */
+
+Node* constructDLL(vector<int>& arr) {
+    // Write your code here
+    Node* head = new Node(arr[0]);
+    Node* pre = head;
+    for(int i = 1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        temp->prev = pre;
+        pre->next = temp;
+        pre = temp;
+    }
+    return head;
+}
