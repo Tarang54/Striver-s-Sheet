8a7e9fd95de8df24cda9beb31a5c9f87f2e9f8fd
Tarang Sultania <108229445+Tarang54@users.noreply.github.com>
2024-03-24 02:34:49 +0530

    Create step6.1-prg4.cpp

---
+++ step6.1-prg4.cpp
@@ -0,0 +1,39 @@
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
+            }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+
+
+*****************************************************************/
+
+int length(Node *head)
+{
+	//Write your code here
+    int cnt = 0;
+    while(head!=NULL){
+        cnt++;
+        head = head->next;
+    }
+    return cnt;
+}
