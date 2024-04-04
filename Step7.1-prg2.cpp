d519ae90dc7bcd7729b3ec487a9353217301e986
Tarang Sultania <108229445+Tarang54@users.noreply.github.com>
2024-04-04 18:55:12 +0530

    Create Step7.1-prg2.cpp

---
+++ Step7.1-prg2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == 0){
+            return 1;
+        }
+        if(n<0){
+            n = abs(n);
+            x = 1/x;
+        }
+        if(n%2 == 1){
+            return x*myPow(x,n-1);
+        }else{
+            return myPow(x*x, n/2);
+        }
+    }
+};
